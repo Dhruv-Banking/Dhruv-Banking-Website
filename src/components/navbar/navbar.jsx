b33afd3cff602330dfd43d4ff3f50ee4c6541ee8
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-01-24 12:14:40 +1300

    Feature: Navabar component done!

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,26 @@
+import "./navbar.css";
+
+import { Link } from "react-router-dom";
+
+function Navbar(props) {
+  return (
+    <nav>
+      <Link to="#" className="DHB__Header-Nav_Icon">
+        Dhruv Banking™️
+      </Link>
+
+      <ul>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link className="DHB__Header-Nav_Lock" to="/login">
+            Login<i className="fa-solid fa-lock"></i>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
